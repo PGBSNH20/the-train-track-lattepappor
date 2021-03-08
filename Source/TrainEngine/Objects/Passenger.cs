d0a45cbebf817007488c091626b2ec544fa0e451
SPEEDYGONZALES\Sebastian <s.ebrandt@hotmail.com>
2021-03-08 15:46:30 +0100

    new objects for passengers, traintrack and controllerlog

---
+++ Source/TrainEngine/Objects/Passenger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainEngine.Objects
+{
+    public class Passenger
+    {
+        private string _name;
+        private string _lastName;
+        private int _age;
+        private int _id;
+    }
+}
---
+++ Source/TrainEngine/Objects/TrainTrack.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainEngine.Objects
+{
+    public class TrainTrack
+    {
+        private int _id;
+        private bool isOccuppied;
+
+
+    }
+}
