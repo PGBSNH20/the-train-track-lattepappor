7104472b804efd67e0a581fa5c00ad7c8679cbb9
SPEEDYGONZALES\Sebastian <s.ebrandt@hotmail.com>
2021-03-13 23:07:36 +0100

    Added enum to give names to each tracktype (ie - is "Track", = is "Crossing" etc)

---
+++ Source/TrainEngine/Utils/TrackIdentity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainEngine.Utils
+{
+    public enum TrackIdentity : int
+    {
+        Void = 32, //Space char int value = 32
+        StartStation = '*',
+        Track = '-',
+        Crossing = '=',
+        SwitchRight = '>',
+        SwitchLeft = '<',
+        Train = 'T',
+        Station1 = '1',
+        Station2 = '2',
+        Station3 = '3',
+        Station4 = '4'
+    }
+}
