82c0484efa1259b07c87723ea7d3bb27e61e4091
SPEEDYGONZALES\Sebastian <s.ebrandt@hotmail.com>
2021-03-13 23:13:45 +0100

    added a char grid that'll act as our traintrack visualizer, added position to switches and a method to fetch all the switches in the grid as well as assigning them their position on the grid

--- Source/TrainEngine/Objects/TrainTrack.cs
+++ Source/TrainEngine/Objects/TrainTrack.cs
@@ -1,22 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace TrainEngine.Objects
 {
     public class TrainTrack
     {
-        public int Id;
-        public bool IsOccuppied;
+        public static char[][] TrackGrid;
+        //This is the real grid without trains, we'll use this to refresh the train positions on TrackGrid.
+        public static char[][] READONLYGRID => FileIO.DeserializeTrainTrack(@"C:\Users\Sebastian\source\repos\the-train-track-lattepappor\Source\TrainEngine\Data\traintrack.txt");
+         
+        public TrainTrack()
+        { 
+            TrackGrid = READONLYGRID;
+        }
+
+        public static void RefreshTrack()
+        {
+            //Refresh the grid so we can update the train positions elsewhere
+            TrackGrid = READONLYGRID;
+        }
     }
 
-    public class LevelCrossing : TrainTrack
+    public class LevelCrossing
     {
+        public int Id;
         public bool IsOpen = true;
     }
 
-    public class Switch : TrainTrack
+    public class Switch
     {
         public bool DirectionLeft = true;
+        public Position Position;
+        public static List<Switch> Switches;
+
+        public static List<Switch> GetSwitches()
+        {
+            List<Switch> result = new List<Switch>();
+
+            //loop through the read-only grid and find out what positions < and > are located at, then make a new switch with that pos and add to result
+            for(int y = 0; y < TrainTrack.READONLYGRID.Length; y++)
+            {
+                for(int x = 0; x < TrainTrack.READONLYGRID[y].Length; x++)
+                {
+                    if(TrainTrack.READONLYGRID[y][x] == '<' || TrainTrack.READONLYGRID[y][x] == '>')
+                    {
+                        Switch @switch = new Switch()
+                        {
+                            DirectionLeft = true,
+                            Position = new Position(y, x)
+                        };
+                        result.Add(@switch);
+                    }
+                }
+            }
+            return new List<Switch>();
+        }
     }
+
+
 }
